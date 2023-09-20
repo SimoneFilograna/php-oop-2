ad6122afc6a829920cf355d95ea22dde2e3b7b63
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-09-20 19:34:21 +0200

    add-all-elements

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     require_once __DIR__."/models/Giochi.php";
     require_once __DIR__."/models/Cucce.php";
     require_once __DIR__."/models/Cane.php";
+    require_once __DIR__."/models/Gatto.php";
 
     $cane = new Cane();
     $gatto = new Gatto();
@@ -25,8 +26,45 @@
         _img: "https://arcaplanet.vtexassets.com/arquivos/ids/227172/yes-pallina-peluche-rosa-con-coda-cm-4-5x12.jpg?v=1781482722",
         _prezzo: 10);
 
+    $cuccia1 = new Cucce(
+        _nome: "Cuccia Rettangolare Zig Zag", 
+        _categoria: $cane->getImg(), 
+        _img: "https://arcaplanet.vtexassets.com/arquivos/ids/273257/LUNA-TEO-CUCCIA-RETT.-CM.-60-ZIG-ZAG.jpg?v=1781603512",
+        _prezzo: 40);
+
+    $cuccia2 = new Cucce(
+        _nome: "Cuccetta di Natale Elf Hat", 
+        _categoria: $gatto->getImg(), 
+        _img: "https://arcaplanet.vtexassets.com/arquivos/ids/276486/croci-cuccetta-di-natale-per-cane-elf-hat.jpg?v=1779546400",
+        _prezzo: 50);
+
+    $cuccia3 = new Cucce(
+        _nome: "Cuccia Igloo Rosa", 
+        _categoria: $gatto->getImg(), 
+        _img: "https://arcaplanet.vtexassets.com/arquivos/ids/268671/zolux-cuccia-igloo-rosa.jpg?v=1779754884",
+        _prezzo: 20);
+
+    $cibo1 = new Cibo(
+        _nome: "Natural Trainer Gatto Sterilised Salmone", 
+        _categoria: $gatto->getImg(), 
+        _img: "https://arcaplanet.vtexassets.com/arquivos/ids/248329/trainer-natural-gatto-sterilized-salmone.jpg?v=1781629878",
+        _prezzo: 10);
+    
+    $cibo2 = new Cibo(
+        _nome: "Monge Sterilised ricco di Pollo", 
+        _categoria: $gatto->getImg(), 
+        _img: "https://arcaplanet.vtexassets.com/arquivos/ids/271838/400.jpg?v=1781620854",
+        _prezzo: 10);
+    
+    $cibo3 = new Cibo(
+        _nome: "Purina Pro Plan Veterinary Diets Hypoallergenic HA Cane", 
+        _categoria: $cane->getImg(), 
+        _img: "https://arcaplanet.vtexassets.com/arquivos/ids/276167/MAIN.jpg?v=1781630151",
+        _prezzo: 10);
     
 
+    
+    
     
 
 
