0b830b8ef44fbdc3d3da04e419d1b330e3c61ca2
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-09-22 09:48:04 +0200

    add-require

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     require_once __DIR__ . "/models/Cane.php";
     require_once __DIR__ . "/models/Gatto.php";
     require_once __DIR__ . "/db/list.php";
+    require_once __DIR__ . "/traits/NecessaryData.php";
 
     // $cane23 = new Cane();
     // $cane5 = new Giochi("23", $cane->getImg(), "immagine figa", 23);
